fd59098aaf97497916a6ad76698e804fb8d0bb6a
govind kumar <gk4051668@gmail.com>
2022-12-11 21:38:59 +0530

    added files

--- linked-list/linkedlist.js
+++ linked-list/linkedlist.js
@@ -26,7 +26,28 @@ class LinkedList {
 }
 
 let newList = new LinkedList();
-newList.push(13);
-newList.push(14);
+newList.push(1);
+newList.push(1);
+newList.push(2);
 
-console.log(newList);
+function removeDuplicates(list) {
+  let head = list;
+  if (!head) return head;
+
+  let prev = head;
+  let next = prev.next;
+  while (prev.next!=null) {
+    console.log(prev,next)
+    if(prev.val==next.val){
+      let newNext = next.next;
+      prev.next = newNext;
+    }else{
+      prev = prev.next;
+      next = next.next;
+    }
+  }
+  return head;
+}
+
+let ans = removeDuplicates(newList);
+console.log(ans);
